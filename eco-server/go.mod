cf46b180ad545229120f0196b979f03f56863407
washi4 <wayne.shi1@ingka.ikea.com>
2024-12-06 20:55:32 +0800

    add echo-server

---
+++ eco-server/go.mod
@@ -0,0 +1,3 @@
+module gitee.com/zmberg/samples/argo-demo
+
+go 1.19
---
+++ eco-server/main.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", handleRequest)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	// 在这里执行需要消耗CPU时间的操作
+	for i := 0; i < 50000000; i++ {
+		_ = i * i
+	}
+	elapsed := time.Since(start)
+	fmt.Fprintf(w, "Request processed in %s", elapsed)
+}
